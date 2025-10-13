bcb5c4982325e9596513b2dd023bc294bef411b9
zablink <zablinkapp@gmail.com>
2025-10-13 23:22:16 +0700

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,10 +61,10 @@ export default function RootLayout({
       <body style={{ margin: 0, padding: 0, fontFamily: 'system-ui, sans-serif' }} >
         <SessionProvider>
           <AuthProvider>
-          </AuthProvider>
           <AppLayout>
             {children}
           </AppLayout>
+          </AuthProvider>
         </SessionProvider>
       </body>
     </html>
