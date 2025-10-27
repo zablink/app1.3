346cc6a8b2ace5dbf0fec063becc036ca20b8dd4
zablink <zablinkapp@gmail.com>
2025-10-28 01:13:45 +0700

    logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 // src/components/layout/Navbar.tsx
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { useSession, signOut } from "next-auth/react";
