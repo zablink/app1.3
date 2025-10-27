9eb8ed5719f5cd8bf30bcc7bae113ef88eda43e8
zablink <zablinkapp@gmail.com>
2025-10-28 01:22:06 +0700

    logo

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -5,7 +5,6 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import AppLayout from "@/components/AppLayout";
 
 type Shop = {
   id: number;
