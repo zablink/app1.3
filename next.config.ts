dd31f5751837c74b6e0f23d169b88b73a9f72715
zablink <zablinkapp@gmail.com>
2025-11-02 17:11:34 +0700

    Fix: lost account avatar / edit next-config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,50 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  images: {
+    images: {
+    remotePatterns: [
+      // Google
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      // Facebook
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
+      // LINE
+      {
+        protocol: 'https',
+        hostname: 'profile.line-scdn.net',
+      },
+      // Twitter/X
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+      },
+      // TikTok
+      {
+        protocol: 'https',
+        hostname: 'p16-sign-sg.tiktokcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p16-amd-va.tiktokcdn.com',
+      },
+      // GitHub (ถ้าเพิ่มในอนาคต)
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
