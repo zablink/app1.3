84cae9da608d0f9acf0b97b697914f4b648d6522
zablink <zablinkapp@gmail.com>
2025-11-04 23:30:13 +0700

    fix: auth.ts add cookie

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -130,6 +130,20 @@ export const authOptions: NextAuthOptions = {
   // ✅ Secret key สำคัญมาก!
   secret: process.env.NEXTAUTH_SECRET,
 
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
+
   // Pages
   pages: {
     signIn: "/signin",
