0d5f43c22213c80e10c62f358ad5b164addb85db
zablink <zablinkapp@gmail.com>
2026-01-18 00:40:57 +0700

    after Cursor analyze

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/lib/prisma';
 import bcrypt from 'bcryptjs';
 
